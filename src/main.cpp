47abc8a2d161254c285533d90296da4d5c02e914
essentialblend <ambiverb@gmail.com>
2023-10-18 00:46:32 +0100

    rendering lines

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
+#pragma warning( disable : 4100 ) 
 
 #include <GLAD/glad.h>
 #include <GLFWLib/glfw3.h>
 
 #include <iostream>
+
+#include <../gameOfLife2D/src/headers/VAO.h>
+#include <../gameOfLife2D/src/headers/VBO.h>
+
+#include <../gameOfLife2D/src/headers/shader.h>
 
 constexpr unsigned int WIN_HEIGHT = 600;
 constexpr unsigned int WIN_WIDTH = 800;
@@ -13,7 +19,7 @@ void processInput(GLFWwindow* window);
 
 int main()
 {
-	/*Initialization*/
+	/*----Initialization----*/
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -38,6 +44,32 @@ int main()
 		std::cout << "FAILED: GLAD init!" << std::endl;
 		return -1;
 	}
+	/*---- ----*/
+
+	/*Line points*/
+	float linePoints[] = {
+		-1.f, 0.f, 0.f,
+		+1.f, 0.f, 0.f,
+	};
+
+	/*Line VAO/VBO*/
+	VAO lineVAO;
+	VBO lineVBO;
+
+	lineVAO.bind();
+	lineVBO.bind();
+	glBufferData(GL_ARRAY_BUFFER, sizeof(linePoints), linePoints, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	/*LineShader*/
+	Shader lineShader("src/shaders/line.vert", "src/shaders/line.frag");
+	
+
+
+
+
+
 
 	/*Rendering loop*/
 	while (!glfwWindowShouldClose(mainWindow))
@@ -47,8 +79,14 @@ int main()
 		glClearColor(1.f, 1.f, 1.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		lineShader.UseShader();
+		lineVAO.bind();
+		glDrawArrays(GL_LINES, 0, 2);
+		
+		/*---- ----*/
 		glfwSwapBuffers(mainWindow);
 		glfwPollEvents();
+		/*---- ----*/
 	}
 	
 	glfwTerminate();
